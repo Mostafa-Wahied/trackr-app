3bd9f50a73b829838768fa6e71b8fead62be80e8
mostafa-wahied <mostafawahied@gmail.com>
2022-05-27 09:31:55 -0700

    fix test 3

--- test.py
+++ test.py
@@ -15,10 +15,8 @@ def test2():
     assert response.status_code == 200
 
 def test3():
-    def test3():
-        """A dummy docstring."""
+    """A dummy docstring."""
     response = app.test_client().get("/edit")
-    assert b"To Do App" in response.data
+    assert b"To-Do Application" in response.data
     assert b"Todo Title" in response.data
     assert b"Add" in response.data
-
